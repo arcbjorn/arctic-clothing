717426db31dcff69169ff6c9e97fcb63a46707fe
Arc <arky.happ@gmail.com>
2020-06-29 18:35:44 +0300

    1.2.1
     - added Redirect on auth
     - added action types
     - fixed sign-in-sign-up margins

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -61,6 +61,7 @@ class SignUp extends React.Component {
       <div className="sign-up">
         <h2 className="title">I do not have an account</h2>
         <span>Sign up with your email and password</span>
+
         <form className="sign-up-form" onSubmit={this.handleSubmit}>
           <FormInput
             type="text"
--- src/redux/user/user.actions.jsx
+++ src/redux/user/user.actions.jsx
@@ -1,7 +1,7 @@
-/* eslint-disable import/prefer-default-export */
+import { UserActionTypes } from './user.types';
 
 // creating an action for reducer to execute
 export const setCurrentUser = (user) => ({
-  type: 'SET_CURRENT_USER',
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
