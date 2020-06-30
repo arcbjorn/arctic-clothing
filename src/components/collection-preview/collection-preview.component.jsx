9a699bc414844bdafe551a4e6c4f5b8328e4154d
Arc <arky.happ@gmail.com>
2020-07-01 01:08:50 +0300

    1.2.3
     - added cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollecttionItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollecttionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
