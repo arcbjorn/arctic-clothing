c89c7a34d7763b2db8431676ae31561959abf7f5
Arc <arky.happ@gmail.com>
2020-07-04 06:18:59 +0300

    1.2.6
     - added checkout page
     - checkout item component
     - added reselect to cart

--- src/redux/cart/cart.selectors.jsx
+++ src/redux/cart/cart.selectors.jsx
@@ -2,6 +2,11 @@ import { createSelector } from 'reselect';
 
 const selectCart = (state) => state.cart;
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden,
+);
+
 export const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems,
@@ -11,6 +16,14 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) => cartItems.reduce(
     (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0,
+  ),
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price,
     0,
   ),
 );
