c706f49ef8c4df1e44ceeaa9f84cb7016589ee73
Arc <arky.happ@gmail.com>
2020-07-06 01:41:33 +0300

    1.3.2
     - added DB config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: 'AIzaSyBEsMqcV18ZhbxviXf03xXrP9oRPIEyfJ0',
+  authDomain: 'arctic-db-87a73.firebaseapp.com',
+  databaseURL: 'https://arctic-db-87a73.firebaseio.com',
+  projectId: 'arctic-db-87a73',
+  storageBucket: 'arctic-db-87a73.appspot.com',
+  messagingSenderId: '689814725093',
+  appId: '1:689814725093:web:cc4a472f3cd8620f290f0c',
 };
 
 firebase.initializeApp(config);
