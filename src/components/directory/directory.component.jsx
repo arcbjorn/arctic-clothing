f0ff48bd42863666bf303f2d5a1bb762fee88ac4
Arc <arky.happ@gmail.com>
2020-07-05 20:13:55 +0300

    1.3.0
     - moved directory&shop data in dedicated reducers
     - created respective selectors
     - updated corresponding components with new redux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
-import SECTIONS_DATA from './sections.data';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sections: SECTIONS_DATA,
-    };
-  }
-
-  render() {
-    return (
-      <div className="directory-menu">
-        {
-          this.state.sections.map(({ id, ...otherSectionProps }) => (
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {
+          sections.map(({ id, ...otherSectionProps }) => (
             <MenuItem key={id} {...otherSectionProps} />
           ))
         }
-      </div>
-    );
-  }
-}
+  </div>
+);
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps)(Directory);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+/* eslint-disable import/named */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
 
 import App from './App';
 import './index.css';
@@ -11,7 +14,9 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
