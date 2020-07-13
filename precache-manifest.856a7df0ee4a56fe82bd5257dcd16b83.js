1c201e648d576848c677a0177785c43461abcaf3
Arc <arky.happ@gmail.com>
2020-07-13 23:22:08 +0300

    Updates

---
+++ precache-manifest.856a7df0ee4a56fe82bd5257dcd16b83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58d79bef6e36ea8970841dbcbad0172",
+    "url": "/arctic-clothing/index.html"
+  },
+  {
+    "revision": "9e01d3b1be5fa891efc7",
+    "url": "/arctic-clothing/static/css/main.a7d79abf.chunk.css"
+  },
+  {
+    "revision": "7631e52f48f9a41ac8c2",
+    "url": "/arctic-clothing/static/js/2.50ec5074.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/arctic-clothing/static/js/2.50ec5074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e01d3b1be5fa891efc7",
+    "url": "/arctic-clothing/static/js/main.ddb44878.chunk.js"
+  },
+  {
+    "revision": "c9ff80e3f4c4d5dfc43d",
+    "url": "/arctic-clothing/static/js/runtime-main.239dab39.js"
+  },
+  {
+    "revision": "78bebd9b88411a23540ae7093286219b",
+    "url": "/arctic-clothing/static/media/logo.78bebd9b.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/arctic-clothing/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
